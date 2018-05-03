7a370421de76223b068c1bf2ded72afce9cf922d
zhouxiaolong <whoiszxl@gmail.com>
2018-05-03 18:08:01 +0800

    permission: 集成logback

--- 权限系统开发/project_code/permission/src/main/java/com/whoiszxl/controller/UserController.java
+++ 权限系统开发/project_code/permission/src/main/java/com/whoiszxl/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.whoiszxl.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,15 @@ import com.whoiszxl.model.SysUser;
 @RestController
 public class UserController {
 	
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	private SysUserMapper sysUserMapper;
 	
 	@GetMapping("/user")
 	public SysUser getUser() {
+		logger.error("UserController.getUser()");
+		logger.info(sysUserMapper.selectByPrimaryKey(1).toString());
 		return sysUserMapper.selectByPrimaryKey(1);
 	}
 }
