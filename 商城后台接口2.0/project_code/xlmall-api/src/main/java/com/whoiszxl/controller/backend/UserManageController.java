87b5fed9d1e938b76e722304297b20a1aa930135
zhouxiaolong <whoiszxl@gmail.com>
2018-05-09 17:03:59 +0800

    商城2.0: 关于jedis的一点小总结

--- 商城后台接口2.0/project_code/xlmall-api/src/main/java/com/whoiszxl/controller/backend/UserManageController.java
+++ 商城后台接口2.0/project_code/xlmall-api/src/main/java/com/whoiszxl/controller/backend/UserManageController.java
@@ -1,5 +1,6 @@
 package com.whoiszxl.controller.backend;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,10 @@ import com.whoiszxl.common.Const;
 import com.whoiszxl.common.ServerResponse;
 import com.whoiszxl.entity.User;
 import com.whoiszxl.service.UserService;
+import com.whoiszxl.utils.CookieUtil;
+import com.whoiszxl.utils.JsonUtil;
+import com.whoiszxl.utils.RedisPoolUtil;
+import com.whoiszxl.utils.RedisShardedPoolUtil;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -31,13 +36,17 @@ public class UserManageController {
 
 	@PostMapping("login")
 	@ApiOperation(value = "后台管理员登录")
-	public ServerResponse<User> login(String username, String password, HttpSession session) {
+	public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse httpServletResponse) {
 		ServerResponse<User> response = userService.login(username, password);
 		if (response.isSuccess()) {
 			User user = response.getData();
 			if (user.getRole() == Const.Role.ROLE_ADMIN) {
 				// 登录的是管理员
-				session.setAttribute(Const.CURRENT_USER, user);
+				//session.setAttribute(Const.CURRENT_USER, user);
+				// 单点登录:写入token
+				CookieUtil.writeLoginToken(httpServletResponse, session.getId());
+				RedisShardedPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()),
+						Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
 				return response;
 			} else {
 				return ServerResponse.createByErrorMessage("不是管理员无法登录");
