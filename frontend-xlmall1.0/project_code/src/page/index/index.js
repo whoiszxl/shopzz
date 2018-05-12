a1be5494643c19aeb1f2232ad8c3abcc6e85465b
zhouxiaolong <whoiszxl@gmail.com>
2018-05-12 18:54:36 +0800

    商城前端主页布局完成

--- frontend-xlmall1.0/project_code/src/page/index/index.js
+++ frontend-xlmall1.0/project_code/src/page/index/index.js
@@ -2,17 +2,31 @@
  * @Author: whoiszxl 
  * @Date: 2018-05-11 10:32:21 
  * @Last Modified by: whoiszxl
- * @Last Modified time: 2018-05-12 12:25:58
+ * @Last Modified time: 2018-05-12 18:40:25
  */
 
 'use strict';
 require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
+require('util/slider/index.js');
 
+
+var templateBanner  = require('./banner.string');
 var navSide = require('page/common/nav-side/index.js');
 var _xl = require('util/xl.js');
 
-navSide.init({
-    name : 'order-list'
+$(function() {
+    // 渲染banner的html
+    var bannerHtml  = _xl.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    // 初始化banner
+    var $slider     = $('.banner').unslider({
+        dots: true
+    });
+    // 前一张和后一张操作的事件绑定
+    $('.banner-con .banner-arrow').click(function(){
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    });
 });
---
+++ frontend-xlmall1.0/project_code/src/util/slider/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: whoiszxl 
+ * @Date: 2018-05-12 18:33:08 
+ * @Last Modified by: whoiszxl
+ * @Last Modified time: 2018-05-12 18:36:57
+ */
+
+'use strict';
+
+require('./index.css');
+require('./unslider.js');
