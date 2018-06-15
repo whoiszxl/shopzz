cd3af3aea06d68dcadefea2ffb0342cffe238803
zhouxiaolong <whoiszxl@gmail.com>
2018-06-15 15:56:33 +0800

    docker下的java项目自动发布脚本

--- productionEnvBuild/docker_rebuild_java.sh
+++ productionEnvBuild/docker_rebuild_java.sh
@@ -5,7 +5,8 @@ kill -9 $(ps -aux | grep 10001 | awk '{print $2}')
 cd ~/AYANAMI && git pull origin master
 
 # 然后重新构建项目
-cd ~/AYANAMI/xlmall-api2.0/project_code/xlmall-api/ && /root/soft/apache-maven-3.2.2/bin/mvn clean package -Dmaven.test.skip=true
+cd ~/AYANAMI/xlmall-api2.0/project_code/xlmall-api/ && /root/soft/apache-maven-3.5.3/bin/mvn clean package -Dmaven.test.skip=true
 
 # 运行项目
 cd ~/AYANAMI/xlmall-api2.0/project_code/xlmall-api/target && nohup java -jar xlmall-api-1.0.0.jar --server.port=10001 &
+
