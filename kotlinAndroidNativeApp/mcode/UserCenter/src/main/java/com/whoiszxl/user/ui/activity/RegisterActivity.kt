c05d53a57fd2f7f2ebbd2c4b5d6f185000267990
zhouxiaolong <whoiszxl@gmail.com>
2018-06-26 16:03:36 +0800

    kotlin android-extensions 插件配置

--- kotlinAndroidNativeApp/mcode/UserCenter/src/main/java/com/whoiszxl/user/ui/activity/RegisterActivity.kt
+++ kotlinAndroidNativeApp/mcode/UserCenter/src/main/java/com/whoiszxl/user/ui/activity/RegisterActivity.kt
@@ -2,12 +2,18 @@ package com.whoiszxl.user.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.whoiszxl.user.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        mRegisterBtn.setOnClickListener {
+            Toast.makeText(this, "注册了", Toast.LENGTH_SHORT).show()
+        }
     }
 }
