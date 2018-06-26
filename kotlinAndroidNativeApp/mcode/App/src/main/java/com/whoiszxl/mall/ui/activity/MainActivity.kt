3fdac99520f69dd965f23256288b1618fc75ab0f
zhouxiaolong <whoiszxl@gmail.com>
2018-06-26 15:49:58 +0800

    kotlin UserCENTER项目配置debug和release环境切换

---
+++ kotlinAndroidNativeApp/mcode/App/src/main/java/com/whoiszxl/mall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.whoiszxl.mall.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.whoiszxl.mall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ kotlinAndroidNativeApp/mcode/UserCenter/src/main/java/com/whoiszxl/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.whoiszxl.user.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.whoiszxl.user.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
