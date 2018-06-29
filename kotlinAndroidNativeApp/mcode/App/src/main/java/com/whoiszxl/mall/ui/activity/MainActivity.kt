2b218d0be42eff6eaaaa3e74b63fd6a3553f985e
zhouxiaolong <whoiszxl@gmail.com>
2018-06-29 18:28:25 +0800

    實現d底部導航欄

--- kotlinAndroidNativeApp/mcode/App/src/main/java/com/whoiszxl/mall/ui/activity/MainActivity.kt
+++ kotlinAndroidNativeApp/mcode/App/src/main/java/com/whoiszxl/mall/ui/activity/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        
     }
 }
