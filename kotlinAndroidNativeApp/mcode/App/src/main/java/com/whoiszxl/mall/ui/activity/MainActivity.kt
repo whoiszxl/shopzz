dcf74cfc5a39eedf39119efada389f77b31eb281
zhouxiaolong <whoiszxl@gmail.com>
2018-06-30 22:29:57 +0800

    jrebel使用和导航bottom的使用

--- kotlinAndroidNativeApp/mcode/App/src/main/java/com/whoiszxl/mall/ui/activity/MainActivity.kt
+++ kotlinAndroidNativeApp/mcode/App/src/main/java/com/whoiszxl/mall/ui/activity/MainActivity.kt
@@ -3,6 +3,10 @@ package com.whoiszxl.mall.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.whoiszxl.mall.R
+import kotlinx.android.synthetic.main.activity_main.*
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        mBottomNavBar.checkMsgBadge(false)
+        mBottomNavBar.checkCartBadge(20)
+
+        Observable.timer(2, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {mBottomNavBar.checkMsgBadge(true)}
+
+        Observable.timer(5, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {mBottomNavBar.checkCartBadge(10)}
     }
 }
